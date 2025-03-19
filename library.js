bbdd2ece28176051394697469651bfa2ee00ee9f
Pietrosor <pietrosorbo@hotmail.it>
2025-03-19 16:26:36 +0100

    Update library.js

--- library.js
+++ library.js
@@ -1,11 +1,23 @@
 const getBooks = function () {
-  const booksURL = "https://striveschool-api.herokuapp.com/books"
+  return "https://striveschool-api.herokuapp.com/books"
 }
-console.log(getBooks)
-fetch(getBooks).then((response) => {
-  if (response.ok) {
-    return response.json()
-  } else {
-    throw new Error("Errore nel recupero del libro")
-  }
-})
+
+fetch(getBooks())
+  .then((response) => {
+    if (response.ok) {
+      return response.json()
+    } else {
+      throw new Error("Errore nel recupero del libro")
+    }
+  })
+  .then((books) => {
+    console.log(books)
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+
+const bookImg = document.createElement("img")
+bookImg.classList.add("card-img-top")
+bookImg.setAttribute("src", getBooks.img)
+bookImg.setAttribute("alt", getBooks.title)
